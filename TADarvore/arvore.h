114854b262753065a1ab13ab558e1c1accf87f84
Dinarte Filho <dinarte_filho@hotmail.com>
2024-03-14 16:48:01 -0300

    update arvore.h

---
+++ TADarvore/arvore.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct arvores Arvores;
+
+
+void Arvores * adicionar_arvores();
+
+Arvores * remover_arvores();
+
+Arvores * alterar_quantidade_frutas(); // alterar quantidade de frutas da arvore
+
+Arvores * listar_arvores(); // buscar arvore por nome
