171581be29661ced9a93c898ad4761f794a09010
Dinarte Filho <dinarte_filho@hotmail.com>
2024-03-14 16:48:23 -0300

    update arvore.c

---
+++ TADarvore/arvore.c
@@ -0,0 +1,28 @@
+#include "arvore.h"
+
+struct arvores {
+    char nome [30];
+    char tipo [20];
+    int quantidade_frutas;
+    int idade_arvore;
+};
+
+void Arvores * adicionar_arvores(int , Arvores * arvores) {
+    char * nome = (char*)malloc(30 * sizeof(char));
+    char * tipo = (char*)malloc(20 * sizeof(char));
+}
+
+int Arvores * remover_arvores() {
+
+    return 0;
+}
+
+int Arvores * alterar_quantidade_frutas() {
+
+    return 0;
+}
+
+int Arvores * listar_arvores() {
+
+    return 0;
+}
