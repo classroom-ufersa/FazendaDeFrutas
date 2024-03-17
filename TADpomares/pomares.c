29596a82ecbc03ae64ae7f0498db115cf0ce9f6b
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2024-03-17 06:25:32 -0300

    Update pomares.c

---
+++ TADpomares/pomares.c
@@ -0,0 +1,23 @@
+#include "pomares.h"
+
+// Estrutura Pomares
+struct pomares {
+    int id;
+    float area;
+    Arvores * arvores;
+};
+
+int Pomares * adicionar_pomares() {
+
+    return 0;
+}
+
+int Pomares * remover_pomares() {
+
+    return 0;
+}
+
+int Pomares * listar_pomares() {
+
+    return 0;
+}
