52d4baacf814282eac00a7ad8c92a27cf7849816
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2024-03-17 06:26:26 -0300

    Update pomares.h

---
+++ TADpomares/pomares.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct pomares Pomares;
+
+
+int Pomares * adicionar_pomares();
+
+int Pomares * remover_pomares();
+
+int Pomares * listar_pomares(); // listar os pomares e suas arvores
