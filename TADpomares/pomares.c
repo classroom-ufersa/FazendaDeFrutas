ed85d180aafec8c07682a854d32c21cfbb16bfc4
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2024-04-14 20:07:28 -0300

    Update pomares.c

--- TADpomares/pomares.c
+++ TADpomares/pomares.c
@@ -1,21 +1,17 @@
 #include "pomares.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 typedef struct pomar{
     int identificacao;
     float area_plantio;
     char arvore_presente[100];
     Pomar * prox_elemento;
-    int total_arvores;
 }Pomar;
 
 Pomar * cria_lista_pomar(void) {
     return NULL;
 }
 
-Pomar * insere_pomar_lista(Pomar * lista, int identificacao, float area_plantio, char arvore_presente[], int total_arvores) {
+Pomar * insere_pomar_lista(Pomar * lista, int identificacao, float area_plantio, char arvore_presente[]) {
     Pomar * novo_elemento = (Pomar*) malloc(sizeof(Pomar));
     if(novo_elemento == NULL) {
         printf("Falha ao alocacar a memoria.\n");
@@ -24,7 +20,6 @@ Pomar * insere_pomar_lista(Pomar * lista, int identificacao, float area_plantio,
     novo_elemento->identificacao = identificacao;
     novo_elemento->area_plantio = area_plantio;
     strcpy(novo_elemento->arvore_presente, arvore_presente);
-    novo_elemento->total_arvores = total_arvores;
 
     if(lista == NULL || identificacao < lista->identificacao) {
         novo_elemento->prox_elemento = lista;
@@ -46,15 +41,14 @@ Pomar * carrega_pomar_arquivo(char arquivo_nome[]){
     FILE * arquivo = fopen(arquivo_nome, "r");
     if(arquivo == NULL){
         printf("Erro ao abrir arquivo.\n");
-        exit(1);
+        exit(1);    
     }
     int identificacao;
     float area_plantio;
     char arvore_presente[100];
-    int total_arvores;
 
-    while(fscanf(arquivo, "%d %f %[^\n] %d", &identificacao, &area_plantio, &arvore_presente, &total_arvores) == 4) {
-        lista = insere_pomar_lista(lista, identificacao, area_plantio, arvore_presente, total_arvores);
+    while(fscanf(arquivo, "%d %f %[^\n]%*c", &identificacao, &area_plantio, &arvore_presente) == 3 ) {
+        lista = insere_pomar_lista(lista, identificacao, area_plantio, arvore_presente);
     }
 
     fclose(arquivo);
@@ -62,13 +56,16 @@ Pomar * carrega_pomar_arquivo(char arquivo_nome[]){
 }
 
 void insere_pomar_arquivo(char arquivo_nome[], Pomar * lista){
-   FILE * arquivo = fopen(arquivo_nome, "w"); 
+   FILE * arquivo = fopen(arquivo_nome, "wt"); 
    if(arquivo == NULL) {
         printf("Erro ao abrir arquivo.\n");
         exit(1);
     }
+    if (lista == NULL) {
+        printf("lista vazia");
+    }
     while(lista != NULL) {
-        fprintf(arquivo, "%d\t%f\t%s\t%s\n", lista->identificacao, lista->area_plantio, lista->arvore_presente, lista->total_arvores);
+        fprintf(arquivo, "%d\t%f\t%s\n", lista->identificacao, lista->area_plantio, lista->arvore_presente);
         lista = lista->prox_elemento;
     }
 
@@ -85,29 +82,49 @@ void libera_lista_pomares(Pomar * lista) {
     }
 }
 
-void adiciona_pomar(char arquivo_nome[]){
-    Pomar * lista = carrega_pomar_arquivo(arquivo_nome);
+void imprimirLista(Pomar *lista) {
+    Pomar *temp = lista;
+    while (temp != NULL) {
+        printf("%d ", temp->identificacao);
+        temp = temp->prox_elemento;
+    }
+    printf("\n");
+}
+
+Pomar * adiciona_pomar(char arquivo_nome[], Pomar * lista){
     int identificacao;
     float area_plantio;
-    char arvore_presente[50];
-    int total_arvores;
+    char arvore_presente[100];
 
     printf("==== ADICIONAR POMAR ====\n");
     printf("Informe a identificacao do pomar: \n");
-    scanf("%d", &identificacao);
+    while (1) {
+        if (scanf("%d", &identificacao) == 1) {
+            while(getchar() != '\n');
+            break;
+        } else {
+            printf("O ID inserido e invalido, insira apenas numeros:\n");
+            while (getchar() != '\n'); 
+        }
+    }
     printf("Informe a area de plantio: \n");
-    scanf("%f", &area_plantio);
+    while (1) {
+        if (scanf("%f", &area_plantio) == 1) {
+            while(getchar() != '\n');
+            break;
+        } else {
+            printf("A area inserida e invalida, insira apenas numeros:\n");
+            while (getchar() != '\n'); 
+        }
+    }
     printf("Informe as arvores que estao no pomar: \n");
     scanf(" %[^\n]", arvore_presente);
-    printf("Informe o total de arvores que estao no pomar:\n");
-    scanf("%d", &total_arvores);
     printf("=========================\n");
 
-    lista = insere_pomar_lista(lista, identificacao, area_plantio, arvore_presente, total_arvores);
+    lista = insere_pomar_lista(lista, identificacao, area_plantio, arvore_presente);
     insere_pomar_arquivo (arquivo_nome, lista);
-    libera_lista_pomares(lista);
-
     printf("Adicao realizada com sucesso.\n");
+    return lista;
 }
 
 void imprime_dados_pomar(Pomar * lista) {
@@ -117,9 +134,8 @@ void imprime_dados_pomar(Pomar * lista) {
     }
     while(lista != NULL) {
         printf("Identificacao: %d\n", lista->identificacao);
-        printf("Area de Plantio: %f\n", lista->area_plantio);
+        printf("Area de Plantio: %.2f\n", lista->area_plantio);
         printf("Arvores Presentes: %s\n", lista->arvore_presente);
-        printf("Total de Arvores: %d\n", lista->total_arvores);
         if(lista->prox_elemento != NULL) {
             printf("\n");
         }
@@ -128,32 +144,26 @@ void imprime_dados_pomar(Pomar * lista) {
     printf("===============================\n");
 }
 
-int remove_pomar(char arquivo_nome[], int id_pomar) {
-    Pomar * lista = carrega_pomar_arquivo(arquivo_nome);
-    Pomar * elemento_atual = lista;
-    Pomar * elemento_remover;
-    int removido = 0;
-
-    if(lista != NULL) {
-        if(lista->identificacao == id_pomar) {
-            elemento_remover = lista;
-            lista = lista->prox_elemento;
-            free(elemento_remover);
-            removido = 1;
-        } else {
-            while(elemento_atual->prox_elemento != NULL) {
-                if(elemento_atual->prox_elemento->identificacao == id_pomar) {
-                    elemento_remover = elemento_atual->prox_elemento;
-                    elemento_atual->prox_elemento = elemento_remover->prox_elemento;
-                    free(elemento_remover);
-                    removido = 1;
-                    break;
-                }
-                elemento_atual = elemento_atual->prox_elemento;
-            }
-        }
-        insere_pomar_arquivo(arquivo_nome, lista);
-        libera_lista_pomares(lista);
+int remove_pomar(char arquivo_nome[], int id_pomar, Pomar ** lista) {
+    Pomar *atual = *lista;
+    Pomar *anterior = NULL;
+    
+    while (atual != NULL && atual->identificacao != id_pomar) {
+        anterior = atual;
+        atual = atual->prox_elemento;
     }
-    return removido;
+
+    if (atual == NULL) {
+        return 0;
+    }
+
+    if (anterior == NULL) {
+        *lista = atual->prox_elemento;
+    } else {
+        anterior->prox_elemento = atual->prox_elemento;
+    }
+
+    free(atual);
+    insere_pomar_arquivo(arquivo_nome, *lista);
+    return 1;
 }
